b8a62a7915e61384c634f6916e998ef6bfc0e4e4
CN <857353825@qq.com>
2018-06-24 22:56:46 +0800

    实现隐秘录像

--- car_android/app/src/main/java/cn/goldlone/car/view/BaseActivity.java
+++ car_android/app/src/main/java/cn/goldlone/car/view/BaseActivity.java
@@ -1,12 +1,35 @@
 package cn.goldlone.car.view;
 
-import android.app.Activity;
+import android.Manifest;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.KeyEvent;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import cn.goldlone.car.R;
 
@@ -16,6 +39,9 @@ import cn.goldlone.car.R;
  * @author : Created by CN on 2018/6/10 09:58
  */
 public class BaseActivity extends AppCompatActivity {
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,4 +61,5 @@ public class BaseActivity extends AppCompatActivity {
         }
         super.onResume();
     }
+
 }
